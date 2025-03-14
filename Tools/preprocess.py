f48c75465951a3d62968b40dd3e00c670a1a6da6
Wither37 <eason37.cs12@nycu.edu.tw>
2025-03-14 17:34:44 +0800

    update preprocess.py

--- Tools/preprocess.py
+++ Tools/preprocess.py
@@ -6,43 +6,25 @@ from textblob import TextBlob
 import nltk
 from tqdm import tqdm
 
-# Ensure necessary resources are downloaded
 nltk.download("stopwords")
 nltk.download("wordnet")
 
-# Load the dataset
 df = pd.read_csv("cleaned_data.csv", header=None, names=["Review", "Star Rating"])
-
-# Initialize tqdm for progress tracking
 tqdm.pandas(desc="Processing")
-
-# Convert text to lowercase
-df["Review"] = df["Review"].str.lower()
-
-# Remove punctuation and special characters
-df["Review"] = df["Review"].progress_apply(lambda x: re.sub(r"[^\w\s]", "", x))
-
-# Remove stopwords
-stop_words = set(stopwords.words("english"))
-df["Review"] = df["Review"].progress_apply(lambda x: " ".join([word for word in x.split() if word not in stop_words]))
-
-# Lemmatization
 lemmatizer = WordNetLemmatizer()
+stop_words = set(stopwords.words("english"))
+
+# Convert text to lowercase, remove punctuation, special characters, stopwords, and lemmatization
+df["Review"] = df["Review"].str.lower()
+df["Review"] = df["Review"].progress_apply(lambda x: re.sub(r"[^\w\s]", "", x))
+df["Review"] = df["Review"].progress_apply(lambda x: " ".join([word for word in x.split() if word not in stop_words]))
 df["Review"] = df["Review"].progress_apply(lambda x: " ".join([lemmatizer.lemmatize(word) for word in x.split()]))
 
-# Add Sentiment Score
+# additional features
 df["Sentiment"] = df["Review"].apply(lambda x: TextBlob(x).sentiment.polarity)
-
-# Add Word Count
 df["Word Count"] = df["Review"].progress_apply(lambda x: len(x.split()))
-
-# Add Character Count
 df["Character Count"] = df["Review"].progress_apply(lambda x: len(x))
 
-# Reorder columns to move 'Star Rating' to the last
 df = df[["Review", "Word Count", "Character Count", "Sentiment", "Star Rating"]]
-
-# Save the preprocessed dataset
 df.to_csv("preprocessed_2.csv", index=False)
-
 print("Text preprocessing complete. Processed dataset saved as 'preprocessed_2.csv'.")
